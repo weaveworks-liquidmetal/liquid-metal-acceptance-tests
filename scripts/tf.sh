05793587aee4b12523862d40950e5bfbe3210869
Claudia Beresford <claudiaberesford@gmail.com>
2022-07-29 12:01:22 +0100

    feat: Add terraform to create test infra
    This commit duplicates some of the tf setup I had in my demo env. This
    is fine for now, I will modularise this later so we don't have almost
    the same thing twice but my priority right now is getting this merged
    and running in CI.
    
    The `./scripts/tf.sh` has 3 options.
    - `-v` will produce a vars.json for subsequent terraform commands to
      use. It will check for a metro with capacity as well as some other
      validation.
    - `-u` (up) will run `terraform apply` with `--auto-approve`.
    - `-d` (down) will run `terraform destroy` with `--auto-approve`.
    
    The infra which is created on Equinix is almost the same as that which I
    use for my demos. The main change is that there is no VPN set up. This
    is because the VPN I used required a manual step (clicking a button in
    Tailscale to approve subnet routes) which would not work here as we want
    everything automated. The VPN was needed in my demo so that CAPMVM
    clusters could be created from a local workstation without a) exposing
    the infra publicly, and b) without having to reserve a public address to
    serve as workload clusters' VIP LB address.
    
    There are likely other, automatable ways to do this here, but I decided
    not to get into that right now. So the tests which will eventually be
    run against these Equinix hosts will be running within an Equinix
    machine themselves for the immediate future.

---
+++ scripts/tf.sh
@@ -0,0 +1,99 @@
+#!/usr/bin/env bash
+
+new_vars() {
+	local key=keys/lm-ed
+	local device="${DEVICE:=c3.small.x86}"
+	local device_count="${DEVICE_COUNT:=2}"
+	local flintlock_version="${FLINTLOCK_VERSION:=latest}"
+	local project_name="${PROJECT_NAME:=liquid-metal-acceptance-tests}"
+
+	dir="$(dirname "$(realpath "$0")")"
+
+	# we increment the count before the capacity check, because
+	# the device_count only includes the devices used to run flintlock,
+	# whereas we need to check for capacity with the dhcp/nat device
+	# included.
+	((device_count++))
+	metro="$("$dir"/check.py "$device" "$device_count")"
+	if [ "$metro" == "" ]; then
+		exit 1
+	fi
+	# # put the count back afterwards
+	((device_count--))
+
+	mkdir keys || true
+	rm keys/* || true
+	ssh-keygen -q -f "$key" -t ed25519 -N ""
+	public_key=$(cat $key.pub)
+	private_key_path="$(dirname "$dir")/$key"
+
+	jq --arg org "$METAL_ORG_ID" \
+		--arg token "$METAL_AUTH_TOKEN" \
+		--arg pub_key "$public_key" \
+		--arg priv_key "$private_key_path" \
+		--arg metro "$metro" \
+		--arg device "$device" \
+		--arg host_device_count "$device_count" \
+		--arg fl_version "$flintlock_version" \
+		--arg prj_name "$project_name" \
+		'.org_id = $org |
+		.metal_auth_token = $token |
+		.public_key = $pub_key |
+		.private_key_path = $priv_key |
+		.metro = $metro |
+		.host_device_count = $host_device_count |
+		.flintlock_version = $fl_version |
+		.project_name = $prj_name |
+		.server_type = $device' \
+		./terraform/terraform.tfvars.example.json > ./terraform/terraform.tfvars.json
+}
+
+tf_up() {
+	pushd terraform || exit 1
+	terraform init
+	terraform plan --out ../apply.tf
+	terraform apply --auto-approve ../apply.tf
+	popd || exit 1
+}
+
+tf_down() {
+	pushd terraform || exit 1
+	terraform destroy --auto-approve
+	popd || exit 1
+}
+
+if [ $# = 0 ]; then
+	echo "No command provided. Use '--vars', '--up' or '--down'"
+	exit 1
+fi
+
+if [[ -z "${METAL_AUTH_TOKEN}" ]]; then
+	echo "METAL_AUTH_TOKEN required"
+	exit 1
+fi
+
+if [[ -z "${METAL_ORG_ID}" ]]; then
+	echo "METAL_ORG_ID required"
+	exit 1
+fi
+
+while [ $# -gt 0 ]; do
+	case "$1" in
+	-v | --vars)
+		new_vars || exit 1
+		;;
+	-u | --up)
+		tf_up || exit 1
+		;;
+	-d | --down)
+		tf_down || exit 1
+		;;
+	-*)
+		echo "Unknown arg: $1. Use --vars, --up or --down"
+		;;
+	*)
+		break
+		;;
+	esac
+	shift
+done
