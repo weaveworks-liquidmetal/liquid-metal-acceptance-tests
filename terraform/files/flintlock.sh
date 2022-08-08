9dade7b9d445aff3228e122d885234a3d0da1203
Claudia Beresford <claudiaberesford@gmail.com>
2022-08-08 11:09:30 +0100

    fix: Get the nightly build working
    1. Quote the args going into the make target. This is so that if they
       are empty (ie values are being left to defaults) they show up as `""`
       and the calling binary wont complain about flags needing to be set.
    1. Ensure that if a step in the Makefile fails the target exits 1. Doing
       with with `|| exit 1` for now, which hopefully will be enough.
    1. Pin the version of Containerd to 1.6.6. The recent 1.6.7 release
       requires `gcc-11`. Getting that version sent me down a bit of a
       rabbit hole. `11` is not "natively" available on Ubuntu 20.04 (you
       can get it by adding the apt testing repo, but it is just a bit
       irritating to then swap out gcc 9 for 11 after the fact).
       So I tried bumping to Ubuntu 22.04, which lead to some annoyances,
       first with this new GUI whenever you `apt update` etc which has to be
       deactivated via an env var, and then after that my microvms lost DNS
       resolution and NAT connectivity. I could have only bumped the devices
       running flintlock to 22.04 and left the device serving as the
       management cluster as 20.04, but I decided it was better if we had a
       dedicated ticket to get everything working on 22.04, so I am going to
       come back to this another day. For now I just want to get the
       nightlies working.

--- terraform/files/flintlock.sh
+++ terraform/files/flintlock.sh
@@ -3,4 +3,7 @@
 wget https://raw.githubusercontent.com/weaveworks-liquidmetal/flintlock/main/hack/scripts/provision.sh
 chmod +x provision.sh
 
-./provision.sh all -y --grpc-address "0.0.0.0:9090" --parent-iface "bond0.$VLAN_ID" --insecure
+CONTAINERD="v1.6.6" ./provision.sh all -y \
+  --grpc-address "0.0.0.0:9090" \
+  --parent-iface "bond0.$VLAN_ID" \
+  --insecure
